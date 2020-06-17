01a2e32476a24b6344e5caffa823ce992c4cdb62
immersvin <immersv@gmail.com>
2020-06-17 08:52:04 +0530

    intial commit of game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float shipSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (Vector3.forward*shipSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right*shipSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left*shipSpeed * Time.deltaTime);
+        }
+    }
+}
